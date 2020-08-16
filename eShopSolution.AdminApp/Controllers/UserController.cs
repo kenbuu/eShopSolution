2a69aa1c5fc8e9de0e09b5e59435baead964b162
Ken Buu <nongtrungbuu1903@gmail.com>
2020-08-16 14:56:29 +0700

    commit tạm

---
+++ eShopSolution.AdminApp/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopSolution.AdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+    }
+}
--- eShopSolution.AdminApp/Startup.cs
+++ eShopSolution.AdminApp/Startup.cs
@@ -23,7 +23,17 @@ namespace eShopSolution.AdminApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllersWithViews();
+
+            IMvcBuilder builder = services.AddRazorPages();
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            #if DEBUG
+            if (environment == Environments.Development)
+            {
+                builder.AddRazorRuntimeCompilation();
+            }
+            #endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
